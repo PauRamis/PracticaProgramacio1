c89250659aa7515732411883875500b495d8f04c
Your Name <you@example.com>
2021-11-19 17:30:05 +0100

    start

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Numbers {
-
-
+import java.util.Scanner;
     public static String say(long n) {
+        int ar[] = new int[10];
+        Scanner s = new Scanner(System.in);
+        System.out.println("Insereix un numero");
+        ar[0] = s.nextInt();
+
+
+
         return "";
     }
 
